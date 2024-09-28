e58f2993eb013f17a78c724d05a77bed8c04f94f
Yashjain211 <yj7189447@gmail.com>
2024-09-28 16:20:32 +0530

    Add files via upload

---
+++ Day-1(Cricket World Cup Qualifier).cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int X;
+
+
+    cin >> X;
+    if (X >= 12) {
+        cout << "yes";
+    }
+    else {
+        cout << "No";
+    }
+    return 0;
+
+}
