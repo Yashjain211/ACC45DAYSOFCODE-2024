146022fa60e8d97fc88ac2c3eb088c2d662b37b2
Yashjain211 <yj7189447@gmail.com>
2024-10-04 19:05:44 +0530

    Add files via upload

---
+++ Day-7 (JASSIGNMENTS).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T, X;
+	cin>>T;
+	for(int i=0;i<T;i++){
+	    cin>>X;
+	
+	if(X<=7){
+	    cout<<"yes"<<endl;
+	}
+	else{
+	    cout<<"No"<<endl;
+	}}
+	
+	return 0;
+}	
