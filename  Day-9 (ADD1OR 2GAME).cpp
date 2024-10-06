5617ed84fa60c5c95c76fa8d9fe92fa94543319e
Yashjain211 <yj7189447@gmail.com>
2024-10-06 18:48:20 +0530

    Add files via upload

---
+++  Day-9 (ADD1OR 2GAME).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	 int T;
+	 cin>>T;
+	 while(T--){
+	     int N;
+	     cin>>N;
+	     if(N==1){
+	         cout<<"ALICE"<<endl;
+	     }
+	     else{
+	         cout<<"BOB"<<endl;
+	     }
+	 }
+	 return 0;
+
+}
