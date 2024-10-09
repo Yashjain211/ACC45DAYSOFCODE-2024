07c04dffc63484382e21ffbebc6f987232054097
Yashjain211 <yj7189447@gmail.com>
2024-10-09 19:24:53 +0530

    Add files via upload

---
+++ Day-12 (FLIPCARDS).cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+      int t;
+      cin>>t;
+      while(t--){
+          int n, x;
+          cin>>n>>x;
+          if(x==0||n==x){
+              cout<<0<<endl;
+          }
+          else if(n-x>x){
+              cout<<x<<endl;
+          }
+          else{
+              cout<<n-x<<endl;
+          }
+      }
+      return 0;
+
+}
