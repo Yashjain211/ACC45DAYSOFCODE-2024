a035c9975cc1d1e56bb334db91e5a1d2ca4fff21
Yashjain211 <yj7189447@gmail.com>
2024-10-10 18:21:27 +0530

    Add files via upload

---
+++ Day-13 (EXPERT).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	 int t;
+	 cin>>t;
+	 while(t--){
+	     double x, y;
+	     cin>>x>>y;
+	     if((x*50)/100<=y){
+	         cout<<"YES"<<endl;
+	     }
+	     else{
+	         cout<<"NO"<<endl;
+	     }
+	 }     
+}	     
+	     
+	 
+	 
+
+
