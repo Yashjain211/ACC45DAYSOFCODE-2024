25edd8f43324c144dc2c7e6640a31cf9178befc9
Yashjain211 <yj7189447@gmail.com>
2024-10-26 20:22:59 +0530

    Add files via upload

---
+++ Day-24 (CHEAT).cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    x+=5;
+	    cout<<floor(x*1.0/7)<<endl;
+	}
+	return 0;
+}
+
