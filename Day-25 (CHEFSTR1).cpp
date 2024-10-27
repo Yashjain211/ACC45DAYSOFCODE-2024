97668ac8e958af26eab1fe2dff55053f45620b17
Yashjain211 <yj7189447@gmail.com>
2024-10-27 20:32:36 +0530

    Add files via upload

---
+++ Day-25 (CHEFSTR1).cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    long long int n;
+	    cin>>n;
+	    long long int a[n];
+	    cin>>a[0];
+	    long long int d=0;
+	    for(long long int i=1;i<n;i++)
+	    {
+	        cin>>a[i];
+	        if(a[i]==a[i-1]){
+	        d+=0;}
+	        else{
+	        d+=abs(a[i]-a[i-1])-1;}
+	    }
+	    cout<<d<<endl;
+	}
+}
+
+
+
