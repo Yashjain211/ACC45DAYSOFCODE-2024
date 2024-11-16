47e17445334665b75177ecb23dc6293af3a2553f
Yashjain211 <yj7189447@gmail.com>
2024-11-16 10:06:50 +0530

    Add files via upload

---
+++ Day-45 (PRESENTS).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+	
+	int t;
+	std::cin >> t;
+	while(t--)
+	{
+	     int n;
+	    std::cin >> n;
+	    int count=0;
+	 
+	    std::cout << n-n/5 << std::endl;
+	}
+	return 0;
+}
+
+
